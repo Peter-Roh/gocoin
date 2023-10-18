8e2892995ca1029c60c84c181cb29c38a67cf18b
Peter-Roh <minchul.roh.peter@gmail.com>
2023-10-19 03:15:16 +0900

    blockchain and functions

--- main.go
+++ main.go
@@ -11,13 +11,41 @@ type block struct {
 	prevHash string
 }
 
-func main() {
-	genesisBlock := block{
-		data:     "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks",
-		hash:     "",
-		prevHash: "",
-	}
-	genesisBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(genesisBlock.data+genesisBlock.prevHash)))
+type blockchain struct {
+	blocks []block
+}
 
-	fmt.Println(genesisBlock)
+func (b *blockchain) getPrevHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{
+		data:     data,
+		hash:     "",
+		prevHash: b.getPrevHash(),
+	}
+	newBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(newBlock.data+newBlock.prevHash)))
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) printBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Previous hash: %s\n", block.prevHash)
+		fmt.Println("")
+	}
+}
+
+func main() {
+	chain := blockchain{}
+
+	chain.addBlock("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.printBlocks()
 }
